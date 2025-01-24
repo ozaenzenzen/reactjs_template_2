41e8abf85184dde708a16c0c3e502bfb1c39b70b
Fauzan Akmal Mahdi <fauzan@loyalto.id>
2025-01-24 09:55:43 +0700

    logo navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.addEventListener("scroll", handleScroll);
+    };
+  });
+
+  const navItems = [
+    { link: "Home", path: "home" },
+    { link: "Services", path: "service" },
+    { link: "About", path: "about" },
+    { link: "Product", path: "product" },
+    { link: "Testimonial", path: "testimonial" },
+    { link: "FAQ", path: "faq" },
+  ];
+
+  return (
+    <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
+      <nav>
+        <div>
+          <a
+            href=""
+            className="text-2xl font-semibold flex items-center space-x-3"
+          >
+            <img
+              src={logo}
+              alt="Logo"
+              className="w-10 inline-block items-center"
+            />
+            <span className="text-[#263238]">Fauzan Lab</span>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
