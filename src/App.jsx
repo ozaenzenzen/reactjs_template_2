2e6e560fa41667ac31313d982f138dad5c2a72ea
Fauzan Akmal Mahdi <fauzan@loyalto.id>
2025-01-24 08:16:01 +0700

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold text-brandPrimary">React Native</h1>
-      </div>
+      <Navbar/>
     </>
   );
 }
