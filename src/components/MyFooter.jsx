5a0ebd6607fe772914bb5555235c5454eaeff352
Fauzan Akmal Mahdi <fauzan@loyalto.id>
2025-01-25 09:20:22 +0700

    fix footer

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,20 +1,45 @@
 import React from "react";
+import logo from "../assets/logo3.png";
 
 import { Footer } from "flowbite-react";
-import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+import {
+  BsDribbble,
+  BsFacebook,
+  BsGithub,
+  BsInstagram,
+  BsTwitter,
+} from "react-icons/bs";
+
+// import type { CustomFlowbiteTheme } from "flowbite-react";
+import { Button } from "flowbite-react";
+
+const customTheme = {
+  color: {
+    primary: "bg-red-500 hover:bg-red-600",
+  },
+};
 
 const MyFooter = () => {
   return (
-    <Footer container>
+    <Footer container color="#FFFFFf">
       <div className="w-full">
         <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-          <div>
-            <Footer.Brand
-              href="https://flowbite.com"
-              src="https://flowbite.com/docs/images/logo.svg"
-              alt="Flowbite Logo"
-              name="Flowbite"
-            />
+          <div className="space-y-4 mb-8">
+            <a
+              href=""
+              className="text-2xl font-semibold flex items-center space-x-3"
+            >
+              <img
+                src={logo}
+                alt="Logo"
+                className="w-10 inline-block items-center"
+              />
+              <span className="text-white">Fauzan Lab</span>
+            </a>
+            <div>
+              <p className="text-white mb-1">Copyright &copy; 2025 Fauzanlab</p>
+              <p className="text-white">All rights reserved</p>
+            </div>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
             <div>
@@ -44,11 +69,11 @@ const MyFooter = () => {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright href="#" by="Flowbite™" year={2022} />
           <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-            <Footer.Icon href="#" icon={BsFacebook} />
+            {/* <Footer.Icon href="#" icon={BsFacebook} /> */}
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
             <Footer.Icon href="#" icon={BsGithub} />
-            <Footer.Icon href="#" icon={BsDribbble} />
+            {/* <Footer.Icon href="#" icon={BsDribbble} /> */}
           </div>
         </div>
       </div>
